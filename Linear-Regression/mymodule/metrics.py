efc5c17b2bbaffc278a4b06ccd0cd9fcdf6e3dd7
Zu3zz <3zz@zhengtiangdeMBP.lan>
2018-03-08 22:50:31 +0800

    linear

--- Linear-Regression/mymodule/metrics.py
+++ Linear-Regression/mymodule/metrics.py
@@ -44,4 +44,4 @@ def mean_absolute_error(y_true, y_predict):
 def r2_score(y_true, y_predict):
     """计算y_true和y_predict之间的R Square"""
 
-    return 1 - mean_squared_error(y_true, y_predict) / np.var(y_true)
+    return 1 - mean_squared_error(y_true, y_predict)/ np.var(y_true)
