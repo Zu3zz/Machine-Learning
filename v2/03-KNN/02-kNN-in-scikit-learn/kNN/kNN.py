9a9f012685ac26b462a74a5f3ff33dc7de58267f
Zu3zz <zthzththth@163.com>
2019-08-03 13:46:33 +0800

    finish knn & knn in mnist

--- v2/03-KNN/02-kNN-in-scikit-learn/kNN/kNN.py
+++ v2/03-KNN/02-kNN-in-scikit-learn/kNN/kNN.py
@@ -8,7 +8,7 @@ from math import sqrt
 from collections import Counter
 
 
-class KNNClassfier:
+class KNNClassifier:
 
     def __init__(self, k):
         """初始化kNN分类器"""
@@ -19,8 +19,39 @@ class KNNClassfier:
 
     def fit(self, X_train, y_train):
         """根据训练数据集X_train和y_train训练kNN分类器"""
-        assert X_train.shape[0] == y_train.shape[0]
+        assert X_train.shape[0] == y_train.shape[0], \
+            "the size of X_train must be equal to the size of y_train"
+        assert self.k <= X_train.shape[0], \
+            "the size of X_train must be at least k."
 
+        self._X_train = X_train
+        self._y_train = y_train
+        return self
 
     def predict(self, X_predict):
-        assert self._X_train is not None and self._y_train is not None
+        """给定待预测数据集X_predict，返回表示X_predict的结果向量"""
+        assert self._X_train is not None and self._y_train is not None, \
+            "must fit before predict!"
+        assert X_predict.shape[1] == self._X_train.shape[1], \
+            "the feature number of X_predict must be equal to X_train"
+
+        y_predict = [self._predict(x) for x in X_predict]
+        return np.array(y_predict)
+
+    def _predict(self, x):
+        """给定单个待预测数据x，返回x的预测结果值"""
+        assert x.shape[0] == self._X_train[1], \
+            "the feature number of x must be equal to X_train"
+
+        distances = [sqrt(np.sum((x_train - x) ** 2))
+                     for x_train in self._X_train]
+        nearest = np.argsort(distances)
+
+        topK_y = [self._y_train[i] for i in nearest[:self.k]]
+        votes = Counter(topK_y)
+
+        # votes: ([1:k-x],[0:x]) return 1
+        return votes.most_common(1)[0][0]
+
+    def __repr__(self):
+        return "KNN(k=%d)" % self.k
